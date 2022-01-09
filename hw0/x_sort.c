fa8eab8b7fb96f634d0b96d3e8d7adbc4a4c06bf
cd-public <cd@cs.unc.edu>
2022-01-09 13:26:36 -0800

    adding hw0

---
+++ hw0/x_sort.c
@@ -0,0 +1,49 @@
+# include <stdio.h>
+
+// A simple program to sort an array of integers in C
+
+/* YOUR WORK HERE */
+
+// Input: An array of integers and its length
+// Output: Nothing
+// Side Effect: The input array is sorted from least to greatest
+void sort(int *arr, int len)
+{
+	return;
+}
+
+/* END YOUR WORK */
+
+/* FEEDBACK FUNCTIONS */
+
+// Input: An array of integers and its length
+// Output: 1 if the array is sorted least to greatest, 0 otherwise
+// Side Effect: None
+int is_sorted(int *arr, int len)
+{
+	int i = 0;
+	while (i < (len - 2))
+	{
+		if (arr[i] > arr[i+1])
+		{
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
+
+// The special main function
+
+int main()
+{
+	int test_arr[10] = {8,6,4,2,0,1,3,5,7,9};
+	sort(test_arr, 10);
+	if (is_sorted(test_arr, 10))
+	{
+		printf("Good job!\n");
+	} else {
+		printf("Keep it up!\n");
+	}
+	return 0;
+}
