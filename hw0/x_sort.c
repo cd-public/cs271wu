5312132559f6529f96840bb055285f755e19f5a2
Calvin Deutschbein <ckdeutschbein@willamette.edu>
2022-01-12 11:22:40 -0800

    code from 1/12 class

--- hw0/x_sort.c
+++ hw0/x_sort.c
@@ -5,11 +5,28 @@
 /* YOUR WORK HERE */
 
 // Input: An array of integers and its length
-// Output: Nothing
+// Output: Zero in the case of no errors
 // Side Effect: The input array is sorted from least to greatest
-void sort(int *arr, int len)
+int sort(int *arr, int len) 
 {
-	return;
+	return 0;
+}
+
+// Input: An array, and a location i
+// Output: Zero in the case no swap, one in the case of swap
+// Side Effect: The elements i and i+1 of the array are swapped if 
+//              the ith element is greater than the i+1th element
+int swap(int *arr, int i)
+{
+	int temp;
+	if (arr[i] > arr[i+1])
+	{
+		temp = arr[i];
+		arr[i] = arr[i+1];
+		arr[i+1] = temp;
+		return 1;
+	}
+	return 0;
 }
 
 /* END YOUR WORK */
@@ -34,16 +51,15 @@ int is_sorted(int *arr, int len)
 }
 
 // The special main function
-
 int main()
 {
-	int test_arr[10] = {8,6,4,2,0,1,3,5,7,9};
-	sort(test_arr, 10);
-	if (is_sorted(test_arr, 10))
+	int arr[10] = {8,6,4,2,0,1,3,5,7,9};
+	sort(arr, 10);
+	if (is_sorted(arr, 10))
 	{
 		printf("Good job!\n");
 	} else {
-		printf("Keep it up!\n");
+		printf("Keep it up!: %d, %d, %d\n", arr[0], arr[1], arr[2]);
 	}
 	return 0;
 }
