fcc7a47195bf00efba7e26098ef1736e88461fe7
Calvin <cd-public@users.noreply.github.com>
2022-09-01 09:41:30 -0700

    Update x_sort.c

--- hw0/x_sort.c
+++ hw0/x_sort.c
@@ -1,38 +1,49 @@
 # include <stdio.h>
-# include <stdlib.h>
 
 // A simple program to sort an array of integers in C
 
 /* YOUR WORK HERE */
 
 // Input: An array of integers and its length
-// Output: Zero in the case of no errors
+// Output: Nothing
 // Side Effect: The input array is sorted from least to greatest
-//              This creates the ret array
-int *sort(int *arr, int len) 
+void sort(int *arr, int len)
 {
-	return *arr;
+	return;
 }
 
 /* END YOUR WORK */
 
 /* FEEDBACK FUNCTIONS */
 
+// Input: An array of integers and its length
+// Output: 1 if the array is sorted least to greatest, 0 otherwise
+// Side Effect: None
+int is_sorted(int *arr, int len)
+{
+	int i = 0;
+	while (i < (len - 2))
+	{
+		if (arr[i] > arr[i+1])
+		{
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
+
 // The special main function
-// Creates and maintains the "arr" array
+
 int main()
 {
-	int loc[10] = {8,6,4,2,0,1,3,5,7,9};
-	int *arr = loc;
-	arr = sort(arr, 10);
-	if (is_sorted(arr, 10))
+	int test_arr[10] = {8,6,4,2,0,1,3,5,7,9};
+	sort(test_arr, 10);
+	if (is_sorted(test_arr, 10))
 	{
 		printf("Good job!\n");
 	} else {
-		printf("Keep it up!: %d, %d, %d\n", arr[0], arr[1], arr[2]);
+		printf("Keep it up!\n");
 	}
-	print_arr(loc, 10);
-	print_arr(arr, 10);
-	free(arr);
 	return 0;
 }
