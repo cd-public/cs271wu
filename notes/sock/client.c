865f68ffe942fbc77486c7b48c776f4200c14ec7
Calvin <cd-public@users.noreply.github.com>
2022-10-31 12:42:33 -0700

    Update client.c

--- notes/sock/client.c
+++ notes/sock/client.c
@@ -7,11 +7,9 @@
 
 int main(int argc, char const *argv[]) 
 { 
-	int sock;
+	int sock = socket(DOMAIN, SOCK_STREAM, 0);
 	struct sockaddr_in6 address; 
 	
-	sock = socket(DOMAIN, SOCK_STREAM, 0);
-
 	address.sin6_family = DOMAIN; 
 	address.sin6_port = htons(PORT); // "In practice, only the port field needs to be formatted with htons()"
 	inet_pton(DOMAIN, "::1", &address.sin6_addr); // "::1" is IPv^ "loopback" address
