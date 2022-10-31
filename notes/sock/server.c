cd8fef8f207241e944eb6ef005a8bdc853d8c461
Calvin <cd-public@users.noreply.github.com>
2022-10-31 12:42:53 -0700

    Update server.c

--- notes/sock/server.c
+++ notes/sock/server.c
@@ -8,23 +8,19 @@
 
 int main(int argc, char const *argv[]) 
 { 
-    int sock; 
     struct sockaddr_in6 address; 
-
-    sock = socket(DOMAIN, SOCK_STREAM, 0);
+    int sock = socket(DOMAIN, SOCK_STREAM, 0), opt = 1, addrlen = sizeof(address), connection ; 
+    char buffer[BUFF_SIZE] = {0}; 
 
     address.sin6_family = DOMAIN; 
     address.sin6_port = htons(PORT); // "In practice, only the port field needs to be formatted with htons()"
     address.sin6_addr = in6addr_any; 
-	
-    int opt = 1; // True
+
     setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
     	
     bind(sock, &address, sizeof(address));
     listen(sock, 1);
-    int addrlen = sizeof(address); 
-    int connection = accept(sock, &address, &addrlen); 
-    char buffer[BUFF_SIZE] = {0}; 
+    connection = accept(sock, &address, &addrlen); 
     read(connection, buffer, BUFF_SIZE); 
 	
     printf("%s\n",buffer ); 
