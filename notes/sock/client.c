f54efa6beee527821e5c74d5067af395417da42a
Calvin <cd-public@users.noreply.github.com>
2022-11-03 09:34:18 -0700

    Update client.c

--- notes/sock/client.c
+++ notes/sock/client.c
@@ -1,22 +1,54 @@
-// client.c 
-
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
-#define PORT 8080 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #define DOMAIN AF_INET6
+#define PORT 0xC271
+#define BUFFSIZE 1024
 
-int main(int argc, char const *argv[]) 
-{ 
-	struct sockaddr_in6 address; 
-	int sock = socket(DOMAIN, SOCK_STREAM, 0);
+int main(int argc, char *argv[])
+{
+	// set up variables
+	int sock = socket(DOMAIN, SOCK_STREAM, 0), opt = 1, conx, s = sizeof(struct sockaddr_in6) ;
+	struct sockaddr_in6 addr ;
+	struct sockaddr *add4 ;
+	char buff[BUFFSIZE] = { 0 } ;
 	
-	address.sin6_family = DOMAIN; 
-	address.sin6_port = htons(PORT); // "In practice, only the port field needs to be formatted with htons()"
-	inet_pton(DOMAIN, "::1", &address.sin6_addr); // "::1" is IPv^ "loopback" address
+	// setting up fields in addr
+	addr.sin6_family = DOMAIN ;
+	addr.sin6_port = htons(PORT) ; // this is a number *I* wrote leaving my computer
+	addr.sin6_addr = in6addr_any ;
+	
+	// make testing easier
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+	
+	// network connectivity
+	add4 = (struct sockaddr *)&addr ;
+	if (bind(sock, add4, s))
+	{
+		fprintf(stderr, "Binding failed.\n") ;
+		exit(-1) ; // I am writing "exit" instead of "return" as a matter of style
+	}
+	if (listen(sock, 1))
+	{	
+		fprintf(stderr, "Listen failed.\n") ;
+		exit(-1) ;
+	}
+	conx = accept(sock, add4, &s) ;
+	if (conx == -1)
+	{
+		fprintf(stderr, "Accept failed.\n") ;
+		exit(-1) ;
+	}
 
-	connect(sock, &address, sizeof(address));
+	// read in bits
+	read(conx, buff, BUFFSIZE) ;		
 	
-	write(sock, argv[1], strlen(argv[1]));
+	// transmit bits
+	printf("%s\n", buff) ;
 	
-	return 0; 
-} 
+	return 0 ;
+}
